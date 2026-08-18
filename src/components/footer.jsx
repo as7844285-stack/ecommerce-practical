34d434ea4150699a64484d42897cb6f1f5278a8f
Ankush <as7844285@gmail.com>
2026-08-18 20:56:20 +0530

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,44 +1,54 @@
-import { memo } from 'react';
+import { memo } from "react";
 
 const Footer = () => {
   return (
     <footer className="bottom">
+      <div className="footer-container">
+        <h2>Canvasora</h2>
 
-  <div className="footer-container">
+        <div>
+          <ul>
+            <li>About Canvasora</li>
+            <li>Help / FAQs</li>
+            <li>Careers</li>
+            <li>Internships</li>
+          </ul>
+        </div>
 
-    <h2>e-product</h2>
+        <div>
+          <ul>
+            <li>Advertise</li>
+            <li>Canvasora Insider</li>
+            <li>Digital Catalog</li>
+          </ul>
+        </div>
 
-    <div>
-      <ul>
-        <li>About Marvel</li>
-        <li>Help / FAQs</li>
-        <li>Careers</li>
-        <li>Internships</li>
-      </ul>
-    </div>
+        <div className="logo">
+          <img
+            className="fb"
+            src="https://www.svgrepo.com/show/382721/facebook.svg"
+            alt="Facebook"
+          />
+          <img
+            className="insta"
+            src="https://www.svgrepo.com/show/303145/instagram-2-1-logo.svg"
+            alt="Instagram"
+          />
+          <img
+            className="twitter"
+            src="https://www.svgrepo.com/show/349537/twitter.svg"
+            alt="X"
+          />
+          <img
+            className="yt"
+            src="https://www.svgrepo.com/show/382710/youtube-you-tube-video.svg"
+            alt="YouTube"
+          />
+        </div>
+      </div>
 
-    <div>
-      <ul>
-        <li>Advertise</li>
-        <li>Marvel Insider</li>
-        <li>Digital Comics</li>
-      </ul>
-    </div>
-
-    <div className="logo">
-      <img className='fb' src="https://www.svgrepo.com/show/382721/facebook.svg" alt="Facebook" />
-      <img className='insta' src="https://www.svgrepo.com/show/303145/instagram-2-1-logo.svg" alt="Instagram" />
-      <img className='twitter'src="https://www.svgrepo.com/show/349537/twitter.svg" alt="X" />
-      <img className='yt' src="https://www.svgrepo.com/show/382710/youtube-you-tube-video.svg" alt="YouTube" />
-    </div>
-
-  </div>
-
-  <p className="copyright">
-    © 2026 E-PRODUCT. All Rights Reserved.
-  </p>
-
-</footer>
+      <p className="copyright">© 2026 CANVASORA. All Rights Reserved.</p>
+    </footer>
   );
 };
 
