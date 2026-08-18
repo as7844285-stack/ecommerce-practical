937b3e1ad99213da5ebe7ed8da3438fea67b8da9
Ankush <as7844285@gmail.com>
2026-08-18 22:28:25 +0530

    update url and axios

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  //  || "http://localhost:3000/api/",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api/",
 });
 
 axiosInstance.interceptors.request.use(
